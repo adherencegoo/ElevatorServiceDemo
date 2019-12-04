85a9a58631e382668cf15de67777f61d917533ec
Owen <adherencegoo@gmail.com>
2019-12-04 22:29:41 +0800

    [Dev][UI] Error proof for input: floorCount

--- app/src/main/java/com/xdd/elevatorservicedemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/xdd/elevatorservicedemo/ui/main/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.xdd.elevatorservicedemo.ui.main
 
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import com.xdd.elevatorservicedemo.R
@@ -10,22 +11,27 @@ import com.xdd.elevatorservicedemo.ui.elevator.ElevatorFragment
 
 class MainViewModel : ViewModel() {
     val baseFloor = UserInt()
-    val floorCount = UserInt()
+    val floorCount = UserInt(10)
     val elevatorCount = UserInt(1)
     val animationDurationPerFloor = UserInt(1000)
 
     fun createService(view: View) {
         val activity = view.context as FragmentActivity
 
-        val config = ElevatorService.Config(
-            baseFloor.intValue,
-            floorCount.intValue,
-            elevatorCount.intValue,
-            animationDurationPerFloor.intValue.toLong()
-        )
-        activity.supportFragmentManager.beginTransaction()
-            .replace(R.id.container, ElevatorFragment.newInstance(config))
-            .addToBackStack(null)
-            .commit()
+        if (floorCount.intValue <= 0) {
+            Toast.makeText(activity, "Floor count should be a positive integer", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            val config = ElevatorService.Config(
+                baseFloor.intValue,
+                floorCount.intValue,
+                elevatorCount.intValue,
+                animationDurationPerFloor.intValue.toLong()
+            )
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.container, ElevatorFragment.newInstance(config))
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
