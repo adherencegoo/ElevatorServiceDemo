72f0a41445da7b73e7ec4d46f423ec3cd236c3ee
Owen <adherencegoo@gmail.com>
2019-12-04 22:08:21 +0800

    [Dev][UI] Direction image for ElevatorRoom

---
+++ app/src/main/java/com/xdd/elevatorservicedemo/ui/BindingAdapters.kt
@@ -0,0 +1,17 @@
+package com.xdd.elevatorservicedemo.ui
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.xdd.elevatorservicedemo.R
+import com.xdd.elevatorservicedemo.model.Direction
+
+@BindingAdapter("app:bindDirection")
+fun ImageView.bindDirection(direction: Direction?) {
+    setImageResource(
+        when (direction) {
+            Direction.UP -> R.drawable.direction_up
+            Direction.DOWN -> R.drawable.direction_down
+            else -> android.R.color.transparent
+        }
+    )
+}
